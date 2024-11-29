4a0afa302ea98e67867df2a0cd07e1f0b40de863
zainabn096 <zainabnaveed2512@gmail.com>
2024-11-29 17:17:59 +0500

    add link

--- screens/partials/footer.php
+++ screens/partials/footer.php
@@ -3,7 +3,7 @@
         <div class="text-dark text-center mb-1">  
         <h5 class=""> Where words fail, music speaks.</h5></div>
          <div class="footer-copyright text-dark text-center py-3"> © 2020 Copyright:
-       <a href="../../index.php"> E-music.com</a>
+       <a href="https://www.instagram.com/azsolutions.web/"> Az Solutions</a>
        </div>
       </div> 
 </footer>
